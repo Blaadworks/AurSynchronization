87574131b46ca1d0ce5cc24ac95bea4406b494c2
Blaadick <lablaadick@gmail.com>
2025-05-26 09:56:04 +0300

    Add license setting

--- README.md
+++ README.md
@@ -1,30 +1,40 @@
 # AurSynchronization
+## Description
+Allows you to easily automate package updates on AUR.
+It may work in a few narrow cases at the moment, but it works.
+
+Job Features:
+- Requires a pre-existing package on AUR
+- Works with only one target — the repository in which the action resides
+- Some settings such as build or deployment logic will have to be changed manually
+
 ## Inputs
-| Name            | Is Required | Default Value     |
-|-----------------|-------------|-------------------|
-| pkg_name        | true        |                   |
-| pkg_desc        | false       | ""                |
-| ssh_key         | true        |                   |
-| commit_username | false       | Release publisher |
-| commit_email    | false       | ""                |
+| Name            | Is Required | Default Value                |
+|:----------------|:------------|:-----------------------------|
+| ssh_key         | true        |                              |
+| pkg_name        | true        |                              |
+| pkg_desc        | false       | **`Not changed`**            |
+| pkg_license     | false       | **`Not changed`**            |
+| commit_username | false       | `Release publisher username` |
+| commit_email    | false       | `Empty`                      |
 
-## Example
-```yml
+## Examples
+```yaml
 name: AUR Synchronization
-
 on:
     release:
         types: [published]
-
 jobs:
     aur-sync:
         runs-on: ubuntu-latest
         steps:
-          - uses: Blaadworks/AurSynchronization@v1.0.0
+        -   uses: Blaadworks/AurSynchronization@v1.0.0
             with:
+                ssh_key: ${{ secrets.SSH_KEY_AUR }}
                 pkg_name: incredible-app
-                pkg_desc: "Brief description of the app"
-                commit_username: AppCreator228
-                commit_email: appcreator228@mail.com
-
 ```
+
+Real examples:
+ - BlaadPapers
+   - [action.yml](https://github.com/Blaadick/BlaadPapers/blob/master/.github/workflows/aur-sync.yml)
+   - [PKGBUILD](https://aur.archlinux.org/cgit/aur.git/tree/PKGBUILD?h=blaadpapers)
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,11 @@ set -euo pipefail
 HOME=/home/synchronizer
 cd ~
 
+
+
 pkgName="$INPUT_PKG_NAME"
 pkgDesc="$INPUT_PKG_DESC"
-sshKey="$INPUT_SSH_KEY"
+pkgLicense="$INPUT_PKG_LICENSE"
 gitBody="$GITHUB_REPOSITORY"
 
 
@@ -13,31 +15,44 @@ gitBody="$GITHUB_REPOSITORY"
 # Setup SSH
 mkdir -p ~/.ssh
 eval "$(ssh-agent -s)"
-echo "$sshKey" | ssh-add -
+echo "$INPUT_SSH_KEY" | ssh-add -
 ssh-keyscan aur.archlinux.org >> ~/.ssh/known_hosts
 
 
 
 # Setup AUR & Git
 git config --global user.name "${INPUT_COMMIT_USERNAME:-$GITHUB_ACTOR}"
-git config --global user.email "${INPUT_COMMIT_EMAIL:-''}"
-git -c init.defaultBranch=master clone ssh://aur@aur.archlinux.org/$pkgName.git
-cd $pkgName
+git config --global user.email "${INPUT_COMMIT_EMAIL:-""}"
+git -c init.defaultBranch=master clone "ssh://aur@aur.archlinux.org/$pkgName.git"
+cd "$pkgName"
 
 
 
 # Edit PKGBUILD
-lastVer=$(curl -s "https://api.github.com/repos/$gitBody/releases/latest" | jq -r '.tag_name' | sed 's/^v//')
+lastVer=$(curl -s "https://api.github.com/repos/$gitBody/releases/latest" | jq -r ".tag_name" | sed "s/^v//")
 curl -s -L "https://github.com/$gitBody/archive/refs/tags/v$lastVer.tar.gz" -o "$pkgName-$lastVer.tar.gz"
 sha256sum=$(sha256sum "$pkgName-$lastVer.tar.gz" | cut -d ' ' -f1)
 rm "$pkgName-$lastVer.tar.gz"
 
+repoName=$(echo "$gitBody" | cut -d '/' -f2)
+
+if grep -q "^_reponame=" PKGBUILD; then
+    sed -i -E "s|^_reponame=.*|_reponame=\"$repoName\"|" PKGBUILD
+else
+    sed -i "/^pkgname=/i _reponame=\"$repoName\"" PKGBUILD
+fi
+
 sed -i -E "s|^pkgver=.*|pkgver=\"$lastVer\"|" PKGBUILD
 sed -i -E "s|^sha256sums=.*|sha256sums=(\"$sha256sum\")|" PKGBUILD
 sed -i -E "s|^url=.*|url=\"https://github.com/$gitBody\"|" PKGBUILD
+sed -i -E "s|^source=.*|source=(\"$pkgName-$lastVer.tar.gz::https://github.com/$gitBody/archive/refs/tags/v$lastVer.tar.gz\")|" PKGBUILD
 
-if [[ -z "$pkgDesc" ]]; then
+if [ -n "$pkgDesc" ]; then
     sed -i -E "s|^pkgdesc=.*|pkgdesc=\"$pkgDesc\"|" PKGBUILD
+fi
+
+if [ -n "$pkgLicense" ]; then
+    sed -i -E "s|^license=.*|license=(\"$pkgLicense\")|" PKGBUILD
 fi
 
 makepkg --printsrcinfo > .SRCINFO
